349535d27a28e5d6b1ce0d8b02bfb6d4df168dc0
Vladislav M <vl.moshkov@gmail.com>
2022-06-22 20:17:41 +0300

    Добавьте файлы проекта.

---
+++ Task_3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Address
+{
+    string city;
+    string street;
+    int house_number;
+    int apartment_number;
+    int index;
+};
+
+void Print_address(const Address &person)
+{
+    cout << "Город: " << person.city << endl << "Улица: " << person.street << endl << "Номер дома: " << person.house_number << endl << "Номер квартиры: " << person.apartment_number << endl
+        << "Индекс: " << person.index << endl;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    Address person[] =
+    {
+    {"Москва", "Арбат", 12, 8, 123456},
+    {"Ижевск", "Пушкина", 59, 143,953769},
+    };
+
+    Print_address(person[0]);
+
+    return 0;
+}
+
